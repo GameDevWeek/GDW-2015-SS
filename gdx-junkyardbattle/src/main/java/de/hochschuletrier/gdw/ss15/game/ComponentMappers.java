bcb1b663962dc62788cf44857b493487b651c587
Ricardo Helms <ricardo.helms@gmail.com>
2015-09-21 16:58:01 +0200

    MoveSystem erstellt

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/ComponentMappers.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/ComponentMappers.java
@@ -14,4 +14,8 @@ public class ComponentMappers {
     public static final ComponentMapper<HealthComponent> health = ComponentMapper.getFor(HealthComponent.class);
     public static final ComponentMapper<DamageComponent> damage = ComponentMapper.getFor(DamageComponent.class);
     public static final ComponentMapper<DeathComponent> death = ComponentMapper.getFor(DeathComponent.class);
+    public static final ComponentMapper<BulletComponent> bullet = ComponentMapper.getFor(BulletComponent.class);
+    public static final ComponentMapper<MetalComponent> metal = ComponentMapper.getFor(MetalComponent.class);
+    public static final ComponentMapper<MoveComponent> move = ComponentMapper.getFor(MoveComponent.class);
+    public static final ComponentMapper<PlayerComponent> player = ComponentMapper.getFor(PlayerComponent.class);
 }
