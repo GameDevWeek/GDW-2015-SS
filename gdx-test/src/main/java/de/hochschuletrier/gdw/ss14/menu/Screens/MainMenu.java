50950f175b6a872242faca119b28949b0c97988e
Nik-UI <bechtn@hochschule-trier.de>
2015-09-21 15:10:12 +0200

    Package Screens im Menu Package with example

---
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/menu/Screens/MainMenu.java
@@ -0,0 +1,12 @@
+package de.hochschuletrier.gdw.ss14.menu.Screens;
+
+import org.lwjgl.opengl.XRandR.Screen;
+
+public class MainMenu extends Screen {
+
+	MainMenu(String name, int width, int height, int freq, int xPos, int yPos) {
+		super(name, width, height, freq, xPos, yPos);
+		// TODO Auto-generated constructor stub
+	}
+
+}
