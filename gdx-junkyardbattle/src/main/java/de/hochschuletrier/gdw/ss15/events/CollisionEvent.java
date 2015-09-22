a22ccd80ef4824e264c6194c56148dee6967eff7
Ricardo Helms <ricardo.helms@gmail.com>
2015-09-22 18:14:19 +0200

    CollisionEvent added

---
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/events/CollisionEvent.java
@@ -0,0 +1,37 @@
+package de.hochschuletrier.gdw.ss15.events;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.utils.SnapshotArray;
+import de.hochschuletrier.gdw.commons.gdx.physix.PhysixContact;
+
+/**
+ * Created by Ricardo on 22.09.2015.
+ */
+public class CollisionEvent {
+
+    private static final SnapshotArray<Listener> listeners = new SnapshotArray<>();
+
+    public static interface Listener
+    {
+        void onCollisionEvent(PhysixContact physixContact);
+    }
+
+    public static void emit(PhysixContact physixContact) {
+        Object[] items = listeners.begin();
+        for (int i = 0, n = listeners.size; i < n; i++) {
+            ((Listener)items[i]).onCollisionEvent(physixContact);
+        }
+        listeners.end();
+    }
+
+    public static void register(Listener listener){
+        listeners.add(listener);
+    }
+    public static void unregister(Listener listener) {
+        listeners.removeValue(listener, true);
+    }
+
+    public static void unregisterAll() {
+        listeners.clear();
+    }
+}
