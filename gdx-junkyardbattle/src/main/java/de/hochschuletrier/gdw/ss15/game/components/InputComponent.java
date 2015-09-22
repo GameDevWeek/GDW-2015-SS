da38f90c9b4951a3d34eaec94cc9aef0ac375eba
Martin Keren <kerenm@hochschule-trier.de>
2015-09-22 14:51:24 +0200

    updateMethode für InputComponent

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/components/InputComponent.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/components/InputComponent.java
@@ -2,6 +2,7 @@ package de.hochschuletrier.gdw.ss15.game.components;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.utils.Pool;
+import de.hochschuletrier.gdw.ss15.game.input.InputMovPaket;
 
 /**
  * Created by Martin on 21.09.2015.
@@ -16,9 +17,20 @@ public class InputComponent extends Component implements Pool.Poolable {
     public int posX;
     public int posY;
 
+    public void update(InputMovPaket inputMovPaket) {
+        this.up = inputMovPaket.up;
+        this.down = inputMovPaket.down;
+        this.left = inputMovPaket.left;
+        this.right = inputMovPaket.right;
+
+        this.posX = inputMovPaket.posX;
+        this.posY = inputMovPaket.posY;
+    }
+
     @Override
     public void reset()
     {
         up = down = left = right = false;
+        posX = posY = 0;
     }
 }
