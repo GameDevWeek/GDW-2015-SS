24dd422ae5184625760ffd4e7d7f88473f43a9e2
Hermann Hockemeier <H.Hermann94@googlemail.com>
2015-09-23 11:05:42 +0200

    System.out.print() in NetworkClientSystem:93 auskommentiert

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/network/NetworkClientSystem.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/network/NetworkClientSystem.java
@@ -90,7 +90,7 @@ public class NetworkClientSystem extends EntitySystem implements EntityListener 
         }
         else if(pack.getPacketId() == PacketIds.EntityUpdate.getValue())
         {//positino update packet
-            System.out.print("update packet received");
+            //System.out.print("update packet received");
             if(pack.getTimestamp()>lastNetworkTimestamp)
             {//synccompoent
                 lastNetworkTimestamp = pack.getTimestamp();
