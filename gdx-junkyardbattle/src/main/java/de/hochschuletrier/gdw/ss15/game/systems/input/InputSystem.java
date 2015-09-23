27cec9dda8324e63c32dee474d299cdf23d6cbf6
David <davidsiepen95@gmail.com>
2015-09-23 13:11:04 +0200

    Kein GamepadSystem mehr

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/input/InputSystem.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/input/InputSystem.java
@@ -6,6 +6,10 @@ import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerListener;
+import com.badlogic.gdx.controllers.PovDirection;
+import com.badlogic.gdx.math.Vector3;
 import de.hochschuletrier.gdw.ss15.Main;
 import de.hochschuletrier.gdw.ss15.game.components.input.InputComponent;
 import de.hochschuletrier.gdw.ss15.game.components.PlayerComponent;
@@ -17,7 +21,7 @@ import de.hochschuletrier.gdw.ss15.game.components.PlayerComponent;
  * Wenn eine Taste gedrueckt wird, wird die entsprechende methode mit dem
  * zugehoerigen keycode aufgerufen.
  */
-public class InputSystem extends IteratingSystem implements InputProcessor {
+public class InputSystem extends IteratingSystem implements InputProcessor, ControllerListener {
 
 
     private boolean up = false;
@@ -159,9 +163,58 @@ public class InputSystem extends IteratingSystem implements InputProcessor {
         // Das InputPaket fuer den Server wird initialisiert
     }
 
+
+    // Controller
+    //---------------------------------------------------------------------------------------
+
+
     @Override
     public void removedFromEngine(Engine engine) {
         super.removedFromEngine(engine);
     }
 
+    @Override
+    public void connected(Controller controller) {
+
+    }
+
+    @Override
+    public void disconnected(Controller controller) {
+
+    }
+
+    @Override
+    public boolean buttonDown(Controller controller, int buttonCode) {
+        return false;
+    }
+
+    @Override
+    public boolean buttonUp(Controller controller, int buttonCode) {
+        return false;
+    }
+
+    @Override
+    public boolean axisMoved(Controller controller, int axisCode, float value) {
+        return false;
+    }
+
+    @Override
+    public boolean povMoved(Controller controller, int povCode, PovDirection value) {
+        return false;
+    }
+
+    @Override
+    public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
+        return false;
+    }
+
+    @Override
+    public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
+        return false;
+    }
+
+    @Override
+    public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
+        return false;
+    }
 }
