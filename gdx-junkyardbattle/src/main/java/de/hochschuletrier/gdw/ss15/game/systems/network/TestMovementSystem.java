2c570548d32f6aa90ace88563a9afdf6e076bd80
Ricardo Helms <ricardo.helms@gmail.com>
2015-09-24 20:42:51 +0200

    mergeConflict fixed

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/network/TestMovementSystem.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/network/TestMovementSystem.java
@@ -12,14 +12,11 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixBodyComponent;
+import de.hochschuletrier.gdw.ss15.events.SoundEvent;
 import de.hochschuletrier.gdw.ss15.events.network.client.SendPacketClientEvent;
 import de.hochschuletrier.gdw.ss15.game.ComponentMappers;
 import de.hochschuletrier.gdw.ss15.game.Game;
-import de.hochschuletrier.gdw.ss15.game.components.InventoryComponent;
-import de.hochschuletrier.gdw.ss15.game.components.HealthComponent;
-import de.hochschuletrier.gdw.ss15.game.components.MoveComponent;
-import de.hochschuletrier.gdw.ss15.game.components.PlayerComponent;
-import de.hochschuletrier.gdw.ss15.game.components.PositionComponent;
+import de.hochschuletrier.gdw.ss15.game.components.*;
 import de.hochschuletrier.gdw.ss15.game.components.input.InputComponent;
 import de.hochschuletrier.gdw.ss15.game.network.Packets.EntityUpdatePacket;
 import de.hochschuletrier.gdw.ss15.game.network.Packets.MovementPacket;
@@ -40,6 +37,7 @@ public class TestMovementSystem extends IteratingSystem{
     private ComponentMapper<MoveComponent> move;
     private ComponentMapper<InputComponent> input;
     private ComponentMapper<InventoryComponent> inventory;
+    private ComponentMapper<SoundEmitterComponent> soundEmitter;
     public TestMovementSystem(Game game, Camera cam)
     {
         super(Family.all(InputComponent.class, MoveComponent.class, InventoryComponent.class).get());
@@ -48,6 +46,7 @@ public class TestMovementSystem extends IteratingSystem{
         move = ComponentMappers.move;
         input = ComponentMappers.input;
         inventory = ComponentMappers.inventory;
+
         
     }
 
@@ -62,7 +61,9 @@ public class TestMovementSystem extends IteratingSystem{
         	InputComponent input = ComponentMappers.input.get(entity);
 	        PositionComponent posc = ComponentMappers.position.get(entity);
 	        InventoryComponent inventory = ComponentMappers.inventory.get(entity);
-	        Vector3 mousepos = camera.unproject(new Vector3(input.posX, input.posY,0));
+            ComponentMapper<SoundEmitterComponent> soundEmitter;
+
+            Vector3 mousepos = camera.unproject(new Vector3(input.posX, input.posY,0));
 	        Vector2 mousepos2 = new Vector2(mousepos.x, mousepos.y);
 	        
 	        mousepos2.sub(new Vector2(posc.x,posc.y));
@@ -81,7 +82,21 @@ public class TestMovementSystem extends IteratingSystem{
        // vectorToAdd.add(velVector);
 
         vectorToAdd.set(input.get(entity).horizontal, input.get(entity).vertical);
-		
-	}
+        if (!vectorToAdd.isZero())
+        {
+            if (ComponentMappers.soundEmitter.has(entity) && !soundEmitter.get(entity).isPlaying) {
+
+                SoundEvent.emit("streetSteps", entity, true);
+                soundEmitter.get(entity).isPlaying = true;
+            }
+        }
+        else
+        {
+            SoundEvent.stopSound(entity);
+            soundEmitter.get(entity).isPlaying = false;
+        }
+
+
+    }
 
 }
