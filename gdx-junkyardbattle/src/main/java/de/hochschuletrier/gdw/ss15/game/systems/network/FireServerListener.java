0d0a87b6f470112c489cfac7b23ee7f0b7546dcd
biwero <biwero@uni-trier.de>
2015-09-24 15:55:41 +0200

    more fixes for projectiles

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/network/FireServerListener.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/network/FireServerListener.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.Vector2;
 import de.hochschuletrier.gdw.commons.gdx.ashley.EntityFactory;
 import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixBodyComponent;
+import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixModifierComponent;
 import de.hochschuletrier.gdw.ss15.events.network.server.NetworkReceivedNewPacketServerEvent;
 import de.hochschuletrier.gdw.ss15.game.ComponentMappers;
 import de.hochschuletrier.gdw.ss15.game.components.InventoryComponent;
@@ -20,7 +21,7 @@ import de.hochschuletrier.gdw.ss15.network.gdwNetwork.data.Packet;
  */
 public class FireServerListener implements NetworkReceivedNewPacketServerEvent.Listener{
 
-    private EntityFactory factory;
+    private EntityFactory<EntityFactoryParam> factory;
 
     public FireServerListener(EntityFactory<EntityFactoryParam> factory){
         this.factory = factory;
@@ -38,11 +39,12 @@ public class FireServerListener implements NetworkReceivedNewPacketServerEvent.L
             float scatter = WeaponComponent.maximumScattering / p;
             Vector2 dir = Vector2.Zero;
 //            System.out.println("received fire package: " + packet.channeltime + "seconds channeld");
-            for (int i = 0; i < WeaponComponent.ShardsPerShot / p; ++i) {
+            for (int i = 0; i < WeaponComponent.ShardsPerShot; ++i) {
                 if(invc.getMetalShards() <= 0){
 //                    System.out.println("not enough metal shards");
                     return;
                 }
+//                System.out.println("shard shot");
 
                 dir.set((float) Math.cos((Math.random() - 0.5f) * scatter),
                         (float) Math.sin((Math.random() - 0.5f) * scatter));
@@ -53,16 +55,16 @@ public class FireServerListener implements NetworkReceivedNewPacketServerEvent.L
                 float projectPlayerDistance = 5.f;
                 float power = 50.f;
                 EntityFactoryParam param = new EntityFactoryParam();
-                Vector2 startPosition = ent.getComponent(PhysixBodyComponent.class).getBody().getPosition().add(dir.setLength(projectPlayerDistance));
+                Vector2 startPosition = phxc.getPosition(); //ent.getComponent(PhysixBodyComponent.class).getBody().getPosition()/*.add(dir.setLength(projectPlayerDistance))*/;
                 param.x = startPosition.x;
                 param.y = startPosition.y;
 
                 invc.addMetalShards(-1);
-                Entity projectile = factory.createEntity("Projectile", param);
-                projectile.getComponent(PhysixBodyComponent.class).applyImpulse(dir.setLength(power));
+                Entity projectile = factory.createEntity("projectile", param);
+                projectile.getComponent(PhysixModifierComponent.class).runnables.add(() -> {
+                    //projectile.getComponent(PhysixBodyComponent.class).applyImpulse(dir.setLength(power));
+                });
             }
-
-
             }catch (ClassCastException e){}
     }
 }
