b78e63456c42d4b0bc317d7c1f9a42ec66d96b83
biwero <biwero@uni-trier.de>
2015-09-24 14:07:41 +0200

    more weapon system additons

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/WeaponSystem.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/WeaponSystem.java
@@ -21,15 +21,13 @@ import de.hochschuletrier.gdw.ss15.game.utils.Timer;
 /**
  * Created by oliver on 21.09.15.
  */
-public class WeaponSystem extends IteratingSystem implements CollisionEvent.Listener {
-
-    Timer timer = new Timer(100); // ms
+public class WeaponSystem extends IteratingSystem {
 
     public WeaponSystem() {
-        super(Family.all(WeaponComponent.class,
-                         PlayerComponent.class,
+        super(Family.all(PlayerComponent.class/*,
+                         WeaponComponent.class,
                          HealthComponent.class,
-                         InputComponent.class).get());
+                         InputComponent.class*/).get());
     }
 
     @Override
@@ -47,10 +45,6 @@ public class WeaponSystem extends IteratingSystem implements CollisionEvent.List
         WeaponComponent wpc = ComponentMappers.weapon.get(entity);
         PositionComponent psc = ComponentMappers.position.get(entity);
         InputComponent input = ComponentMappers.input.get(entity);
-
-
-            // TODO: wait for input and physics
-
         // ask for left click
         if(input.shoot){
         // left button is clicked
@@ -74,33 +68,4 @@ public class WeaponSystem extends IteratingSystem implements CollisionEvent.List
 
     }
 
-
-    @Override
-    public void onCollisionEvent(PhysixContact physixContact) {
-
-    }
 }
-
-
-/*
-                float p = wpc.fireChannelTime / wpc.maximumFireTime + 0.0001f;
-                float scatter = wpc.maximumScattering / p;
-                Vector2 dir = Vector2.Zero;
-                for (int i = 0; i < wpc.maximumShardsPerShot / p; ++i) {
-
-                    dir.set((float) Math.cos((Math.random() - 0.5f) * scatter),
-                            (float) Math.sin((Math.random() - 0.5f) * scatter));
-                    dir.add((float) Math.cos(psc.rotation), (float) Math.sin(psc.rotation));
-                    // create projectile
-                    //Components: Bullet, Damage, Physix
-                    //physix component
-                    float projectPlayerDistance = 5.f;
-                    float power = 50.f;
-                    EntityFactoryParam param = new EntityFactoryParam();
-                    Vector2 startPosition = entity.getComponent(PhysixBodyComponent.class).getBody().getPosition().add(dir.setLength(projectPlayerDistance));
-                    param.x = startPosition.x;
-                    param.y = startPosition.y;
-
-//                Entity projectile = factory.createEntity("Projectile", param);
-//                projectile.getComponent(PhysixBodyComponent.class).applyImpulse(dir.setLength(power));
- */
