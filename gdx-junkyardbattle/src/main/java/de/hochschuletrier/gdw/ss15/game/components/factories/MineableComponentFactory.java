0564f399917ee739c9ac34c0799c459867ff78cd
heilest <heilest@hochschule-trier.de>
2015-09-24 15:39:06 +0200

    Senden von GatherPacket. MineableComponent + Factory hinzugefügt;

---
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/components/factories/MineableComponentFactory.java
@@ -0,0 +1,21 @@
+package de.hochschuletrier.gdw.ss15.game.components.factories;
+
+import com.badlogic.ashley.core.Entity;
+
+import de.hochschuletrier.gdw.commons.gdx.ashley.ComponentFactory;
+import de.hochschuletrier.gdw.commons.utils.SafeProperties;
+
+public class MineableComponentFactory extends ComponentFactory<EntityFactoryParam>{
+
+	@Override
+	public String getType() {
+		return "Mineable";
+	}
+
+	@Override
+	public void run(Entity entity, SafeProperties meta, SafeProperties properties, EntityFactoryParam param) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
