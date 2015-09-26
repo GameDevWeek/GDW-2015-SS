9c1d7c15c54148037fe40b3abc3df63c876f51f6
compix <wladimirkroeker@googlemail.com>
2015-09-26 12:25:58 +0200

    magnet beam effect

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/renderers/EffectAddSystem.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/renderers/EffectAddSystem.java
@@ -7,10 +7,16 @@ import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
 
 import de.hochschuletrier.gdw.ss15.game.ComponentMappers;
+import de.hochschuletrier.gdw.ss15.game.GameGlobals;
 import de.hochschuletrier.gdw.ss15.game.components.PlayerComponent;
 import de.hochschuletrier.gdw.ss15.game.components.PositionComponent;
+import de.hochschuletrier.gdw.ss15.game.components.effects.AttachedParticleEntityComponent;
+import de.hochschuletrier.gdw.ss15.game.components.effects.ParticleEffectComponent;
+import de.hochschuletrier.gdw.ss15.game.components.input.InputComponent;
 import de.hochschuletrier.gdw.ss15.game.components.light.ConeLightComponent;
 import de.hochschuletrier.gdw.ss15.game.components.light.PointLightComponent;
 
@@ -27,20 +33,30 @@ public class EffectAddSystem extends IteratingSystem implements EntityListener {
     @Override
     public void addedToEngine(Engine engine) {
         super.addedToEngine(engine);
-        engine.addEntityListener(Family.all(PlayerComponent.class).get(), this);
+        engine.addEntityListener(Family.all(PlayerComponent.class, InputComponent.class).get(), this);
     }
     
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
         PositionComponent posComp = ComponentMappers.position.get(entity);
         ConeLightComponent coneLightComp = ComponentMappers.coneLight.get(entity);
+        AttachedParticleEntityComponent particleEntityComp = ComponentMappers.particleEntity.get(entity);
+        InputComponent inputComp = ComponentMappers.input.get(entity);
+        
         if(coneLightComp != null) {
             coneLightComp.coneLight.setDirection(posComp.rotation);
+        }
+        
+        if(particleEntityComp != null && particleEntityComp.entity != null) {
+            ParticleEffectComponent particleComp = ComponentMappers.particleEffect.get(particleEntityComp.entity);
+            particleComp.draw = inputComp.gather;
         }
     }
 
     @Override
     public void entityAdded(Entity entity) {
+        PositionComponent entityPos = ComponentMappers.position.get(entity);
+        
         if(!ComponentMappers.coneLight.has(entity)) {
             ConeLightComponent coneLightComp = engine.createComponent(ConeLightComponent.class);
             coneLightComp.set(new Color(0.f, 1.f, 1.f, 0.9f), 15.f, 0.f, 40.f);
@@ -54,6 +70,26 @@ public class EffectAddSystem extends IteratingSystem implements EntityListener {
             entity.add(pointLightComponent);
         }
         
+        if(!ComponentMappers.particleEntity.has(entity)) {
+            Entity particleEntity = engine.createEntity();
+            PositionComponent posComp = engine.createComponent(PositionComponent.class);
+            posComp.x = entityPos.x;
+            posComp.y = entityPos.y;
+            ParticleEffectComponent effectComp = engine.createComponent(ParticleEffectComponent.class);
+            effectComp.particleEffect = new ParticleEffect(GameGlobals.assetManager.getParticleEffect("particle"));
+            effectComp.positionOffsetX = 70.f;
+            effectComp.draw = false;
+            for(ParticleEmitter e : effectComp.particleEffect.getEmitters())
+                e.setAttached(true);
+            
+            particleEntity.add(effectComp);
+            particleEntity.add(entityPos);
+            engine.addEntity(particleEntity);
+            
+            AttachedParticleEntityComponent particleEntityComp = engine.createComponent(AttachedParticleEntityComponent.class);
+            particleEntityComp.entity = particleEntity;
+            entity.add(particleEntityComp);
+        }
     }
 
     @Override
