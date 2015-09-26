3fb3b5f6aa3926e2b5035475f025c507dbd015e0
Ricardo Helms <ricardo.helms@gmail.com>
2015-09-27 00:20:46 +0200

    Pickup Bug gefixt

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/network/PickupSystem.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/systems/network/PickupSystem.java
@@ -38,7 +38,7 @@ public class PickupSystem extends IntervalIteratingSystem implements PickupEvent
             Entity player = physixContact.getOtherComponent().getEntity();
             InventoryComponent invPickup = inventoryComp.get(pickup);
             InventoryComponent invPlayer = inventoryComp.get(player);
-             //System.out.println(invPlayer.getMetalShards());
+             System.out.println(invPlayer.getMetalShards());
 
              if (invPlayer.addMetalShards(invPickup.getMetalShards()) > 0)
              {
