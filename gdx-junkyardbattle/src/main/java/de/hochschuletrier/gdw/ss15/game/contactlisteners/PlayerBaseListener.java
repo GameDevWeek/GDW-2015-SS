1ee0d56a90fe9f2ca5349c3c74d3e439f5a9e706
unknown <rftpool12@rft1208.rz.fh-trier.de>
2015-09-26 14:12:14 +0200

    adjusted PlayerBaseListener

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/contactlisteners/PlayerBaseListener.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/contactlisteners/PlayerBaseListener.java
@@ -2,6 +2,7 @@ package de.hochschuletrier.gdw.ss15.game.contactlisteners;
 
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixContact;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixContactAdapter;
+import de.hochschuletrier.gdw.ss15.events.ComeToBaseEvent;
 import de.hochschuletrier.gdw.ss15.game.ComponentMappers;
 
 public class PlayerBaseListener extends PhysixContactAdapter {
@@ -15,7 +16,7 @@ public class PlayerBaseListener extends PhysixContactAdapter {
             {
 //                System.out.println("basePoint contact (PlayerBaseListener)");
                 // "myEntity" - Player, "otherEntity" - Base
-//                ComeToBaseEvent.emit(contact.getMyComponent().getEntity(), contact.getMyComponent().getEntity());
+                ComeToBaseEvent.emit(contact);
             }
         }
     }
