6575e4e6190369e265da45e9e787015591207c7e
compix <wladimirkroeker@googlemail.com>
2015-09-26 15:20:32 +0200

    Setting team ID, added simple charge effect(still testing)

--- gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/GameConstants.java
+++ gdx-junkyardbattle/src/main/java/de/hochschuletrier/gdw/ss15/game/GameConstants.java
@@ -48,4 +48,6 @@ public class GameConstants {
     public static final float MINING_PER_SECOND = 5;
     public static final float RESPAWN_TIMER = 30000;
     public static final float PROTECTION_DURATION = 4000;
+    public static final float PLAYER_POINT_LIGHT_DISTANCE = 8.f;
+    public static final float PLAYER_POINT_LIGHT_ALPHA = 0.7f;
 }
