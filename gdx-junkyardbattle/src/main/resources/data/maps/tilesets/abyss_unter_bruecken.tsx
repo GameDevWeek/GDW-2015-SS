7fbf80f061c4a9493c81f32f88abae4704fb64c4
unknown <rftpool12@rft1208.rz.fh-trier.de>
2015-09-27 13:47:54 +0200

    fixed and updated maps

---
+++ gdx-junkyardbattle/src/main/resources/data/maps/tilesets/abyss_unter_bruecken.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="abyss_unter_bruecken" tilewidth="128" tileheight="128" tilecount="21">
+ <image source="tileset003_abyss_trans.png" width="896" height="384"/>
+</tileset>
